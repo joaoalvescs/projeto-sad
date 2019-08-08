e70a79386d5119daeb9a411692fe43fba4b06c16
Clovijan <clovijan@gmail.com>
2019-08-07 21:04:57 -0300

    PROCEDIMENTO DAS TABELAS AUX CRIADAS

--- DW/PROCEDIMENTOS_CARGA_TB_AUXILIARES_PRODUTO.sql
+++ DW/PROCEDIMENTOS_CARGA_TB_AUXILIARES_PRODUTO.sql
@@ -1,22 +1,39 @@
-/*CARGA PARA A TB_AUX VENDA_PRODUTO*/
+-----------------------------CARGA PARA A TB_AUX VENDA_PRODUTO-------------------------------------------
+
 CREATE PROCEDURE SP_OLTP_FATO_VENDA_PRODUTO(@data DATETIME)
 AS
 BEGIN
-	DECLARE @cod_compra INT, @dt_compra DATE, @hora_venda TIME, @id_endereco INT, @id_produto INT, @id_pagamento INT,
-			@id_cliente INT, @id_cinema INT, @id_plataforma INT, @id_turno INT, @valor_compra NUMERIC(10,2)
+	DECLARE @cod_compra INT, @dt_venda DATE, @hora_venda TIME, @id_endereco INT, @id_produto INT, @tipo_pag VARCHAR(25),
+			@id_cliente INT, @id_cinema INT, @id_plataforma INT, @valor_compra NUMERIC(10,2)
 
 	DELETE FROM TB_AUX_FATO_VENDA_PRODUTO WHERE DATA_CARGA = @data
 	
-	DECLARE C_FATO_VENDA CURSOR FOR SELECT COM.CODIGO_COMPRA, COM.DATA_COMPRA, COM.HORA_COMPRA, COM.VALOR_COMPRA,COM.ID_CLIENTE,
-										   E.ID_ENDERECO, P.ID_PRODUTO, PAG.ID_PAGAMENTO FROM TB_COMPRA AS COM
-						
-	OPEN C_FATO VENDA
-	FETCH C_FATO_VENDA INTO @cod_compra, @dt_compra, @hora_venda, @valo_compra, @
-	
+	DECLARE C_FATO_VENDA CURSOR FOR SELECT COM.CODIGO_COMPRA, COM.DATA_COMPRA, COM.HORA_COMPRA, COM.VALOR_COMPRA,COM.ID_CINEMA,C.ID_CLIENTE,
+										   COM.TIPO_PAGAMENTO, COM.ID_PLATAFORMA,E.ID_ENDERECO, P.ID_PRODUTO FROM TB_COMPRA AS COM
+										   INNER JOIN TB_CLIENTE AS C ON(COM.ID_CLIENTE = C.ID_CLIENTE) 
+										   INNER JOIN TB_ENDERECO AS E ON(C.ID_ENDERECO = E.ID_ENDERECO)
+										   INNER JOIN TB_COMPRA_PRODUTO AS P ON(P.ID_COMPRA = COM.ID_COMPRA)	
+	OPEN C_FATO_VENDA
+
+	FETCH C_FATO_VENDA INTO @cod_compra, @dt_venda, @hora_venda, @valor_compra,@id_cinema, @id_cliente, @tipo_pag, @id_plataforma,
+							@id_endereco, @id_produto
+
+	WHILE(@@FETCH_STATUS = 0)
+	BEGIN 
+		INSERT INTO TB_AUX_FATO_VENDA_PRODUTO
+		VALUES(@data, @cod_compra, @dt_venda, @hora_venda, @id_endereco, @id_produto, @tipo_pag, @id_cliente, @id_cinema, @id_plataforma,
+			   @valor_compra, 1)
+
+		FETCH C_FATO_VENDA INTO @cod_compra, @dt_venda, @hora_venda, @valor_compra,@id_cinema, @id_cliente, @tipo_pag, @id_plataforma,
+							@id_endereco, @id_produto
+	END
 END
 
 
-/* CARGA PARA O TB_AUX_CLIENTE*/
+
+
+-----------------------------CARGA PARA A TB_AUX_CLIENTE-------------------------------------------
+
 CREATE PROCEDURE SP_OLTP_CLIENTE(@data DATETIME)
 AS
 BEGIN
@@ -26,9 +43,8 @@ BEGIN
 	DECLARE C_CLIENTE CURSOR FOR SELECT C.ID_CLIENTE, P.CPF,P.SEXO,P.NOME, P.SOBRENOME,
 										P.DATA_NASCIMENTO FROM TB_CLIENTE AS C
 										INNER JOIN TB_PESSOA AS P ON (C.ID_PESSOA = P.ID_PESSOA)
-
 	OPEN C_CLIENTE
-	FETCH C_CLIENTE INTO @id,@cpf,@sexo,@nome,@sobrenome,@dt_nascimento
+	FETCH C_CLIENTE INTO @id, @cpf, @sexo, @nome, @sobrenome, @dt_nascimento
 	WHILE(@@FETCH_STATUS = 0)
 		BEGIN 
 			INSERT INTO TB_AUX_CLIENTE
@@ -40,11 +56,8 @@ BEGIN
 	DEALLOCATE C_CLIENTE
 END
 
-SELECT * FROM TB_AUX_CLIENTE
-EXEC SP_OLTP_CLIENTE '20190101'
 
-
-/*----------ENDERECO CLIENTE-------------*/
+-----------------------------CARGA PARA A TB_AUX_ENDERECO_CLIENTE-------------------------------------------
 
 CREATE PROCEDURE SP_OLTP_ENDERECO_CLIENTE(@data DATETIME)
 AS 
@@ -54,11 +67,9 @@ BEGIN
 	SELECT @data, ID_ENDERECO, RUA, NUMERO, CIDADE, BAIRRO,COMPLEMENTO, ESTADO, CEP FROM TB_ENDERECO
 END
 
-SELECT * FROM TB_AUX_ENDERECO_CLIENTE
-EXEC SP_OLTP_ENDERECO_CLIENTE '20190101'
 
+-----------------------------CARGA PARA A TB_AUX_PLATAFORMA-------------------------------------------------
 
-/*------------PLATAFORMA---------------*/
 CREATE PROCEDURE SP_OLTP_PLATAFORMA(@data DATETIME)
 AS
 BEGIN
@@ -66,12 +77,9 @@ BEGIN
 	INSERT INTO TB_AUX_PLATAFORMA
 	SELECT @data, ID_PLATAFORMA, TIPO FROM TB_PLATAFORMA
 END
-EXEC SP_OLTP_PLATAFORMA '20190101'
-SELECT * FROM TB_AUX_PLATAFORMA
 
 
-
-/*------------PRODUTO---------------*/
+-----------------------------CARGA PARA A TB_AUX_PRODUTO----------------------------------------------------
 
 CREATE PROCEDURE SP_OLTP_PRODUTO(@data DATETIME)
 AS
@@ -80,13 +88,10 @@ BEGIN
 	INSERT INTO TB_AUX_PRODUTO
 	SELECT @data, ID_PRODUTO, NOME_PRODUTO,VALOR_PRODUTO FROM TB_PRODUTO
 END
-SELECT * FROM TB_PRODUTO
-EXEC SP_OLTP_PRODUTO '20190101'
 
+-----------------------------CARGA PARA A TB_AUX_PAGAMENTO----------------------------------------------------
 
-
-/*----------------PAGAMENTO-----------------*/
-ALTER PROCEDURE SP_OLTP_PAGAMENTO(@data DATETIME)
+CREATE PROCEDURE SP_OLTP_PAGAMENTO(@data DATETIME)
 AS
 BEGIN
 	DECLARE @codigo INT, @tipo VARCHAR(25), @valor NUMERIC(10,2)
@@ -108,10 +113,8 @@ BEGIN
 	DEALLOCATE C_PAGAMENTO
 END
 
-EXEC SP_OLTP_PAGAMENTO '20190101'
-SELECT * FROM TB_AUX_PAGAMENTO
 
-/*-------------------CINEMA-----------------------*/
+-----------------------------CARGA PARA A TB_AUX_CINEMA-----------------------------------------------
 
 CREATE PROCEDURE SP_OLTP_CINEMA(@data DATETIME)
 AS 
@@ -122,11 +125,7 @@ BEGIN
 END
 
 
-EXEC SP_OLTP_CINEMA '20190101'
-SELECT * FROM TB_AUX_CINEMA
-
-
-/*-------------------TURNO-----------------------------*/
+-----------------------------CARGA PARA A TB_AUX_TURNO-------------------------------------------
 
 CREATE PROCEDURE SP_OLTP_TURNO(@data DATETIME)
 AS
@@ -136,5 +135,3 @@ BEGIN
 	SELECT @data, ID_TURNO, TIPO FROM TB_TURNO
 END
 
-EXEC SP_OLTP_TURNO '20190101'
-SELECT * FROM TB_TURNO
