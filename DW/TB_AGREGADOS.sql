e129544721b31e7771484eabd69c8a5df6ed7654
IgorSantos1996 <igsantos1996@gmail.com>
2019-08-13 18:08:13 -0300

    tabela agregado add

--- DW/TB_AGREGADOS.sql
+++ DW/TB_AGREGADOS.sql
@@ -1,3 +1,5 @@
+ -- Qual o total de produtos vendidos em determinado período
+ -- Qual o valor total de produtos do snack bar vendidos por dias, meses e anos
 CREATE TABLE AGREG_FATO_VENDA_PRODUTO_MENSAL (
 	ID_TEMPO INT NOT NULL REFERENCES DIM_TEMPO(ID_TEMPO),
 	ID_PLATAFORMA INT NOT NULL REFERENCES DIM_PLATAFORMA (ID_PLATAFORMA),
