cca63207f7c40117bd9db1a77653a7f66e1dc8d2
Clovijan <clovijan@gmail.com>
2019-08-14 17:33:33 -0300

    atualização

--- DW/PROCEDIMENTOS_CARGA_TB_AUXILIARES_INGRESSO.sql
+++ DW/PROCEDIMENTOS_CARGA_TB_AUXILIARES_INGRESSO.sql
@@ -10,15 +10,18 @@ END
 CREATE PROCEDURE SP_OLTP_TB_AUX_FILME (@data DATETIME)
 AS
 BEGIN 
-	
+	DELETE FROM TB_AUX_FILME WHERE DATA_CARGA = @data
+	INSERT INTO TB_AUX_FILME
+	SELECT @data, ID_FILME, NOME, GENERO, PRODUTORA, DIRETOR, DURACAO FROM TB_FILME
 END
-
------------------------------CARGA PARA A TB_AUX_PLANO-------------------------------------------
-
 
 -----------------------------CARGA PARA A TB_AUX_PRECO-------------------------------------------
 
-
-
-
 -----------------------------CARGA PARA A TB_AUX_SESSAO-------------------------------------------
+CREATE PROCEDURE SP_OLTP_TB_AUX_SESSAO (@data DATETIME)
+AS 
+BEGIN
+	DELETE FROM TB_AUX_SESSAO WHERE DATA_CARGA = @data
+	INSERT INTO TB_AUX_SESSAO
+	SELECT @data, ID_SESSAO, HORARIO_SESSAO, DATA_SESSAO, LINGUAGEM FROM TB_SESSAO
+END
