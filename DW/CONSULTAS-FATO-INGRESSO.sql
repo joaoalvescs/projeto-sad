d1f7ff9a3e70f2cdadaac3b7e1c1dcf3786b549f
João Alves <joaoalves.cs@gmail.com>
2019-08-19 10:47:15 -0300

    Indicadores

---
+++ DW/CONSULTAS-FATO-INGRESSO.sql
@@ -0,0 +1,11 @@
+/* 1.Quantidade de ingressos vendidos por gênero de filme? */
+/* 2.Qual os dias, meses e anos que mais venderam ingressos? */
+/* 3.Quantidade de ingressos vendidos em determinados períodos por faixa etária? */
+/* 4.Quantidade de ingressos vendidos por plataformas por período? */
+/* 6.Quantidade de pessoas por sessões? */
+/* 7.Quais os filmes que mais venderam ingressos no ano? */
+/* 8.Qual o valor total de ingressos vendidos por dias, meses e anos? */
+/* 10.Qual o valor total de ingressos vendidos por plataformas em dias, meses e anos? */
+/* 11.Qual a quantidade de clientes por determinado bairro, cidade e estado? */
+/* 12.Quais os horários que mais venderam ingressos? */
+/* 13.Total de clientes por plano utilizado? */
