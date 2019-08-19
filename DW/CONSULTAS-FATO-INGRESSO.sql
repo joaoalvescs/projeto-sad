0aacee4c2187c94ddff800f1c7a2d29d5665d9bc
João Alves <joaoalves.cs@gmail.com>
2019-08-19 17:09:15 -0300

    Consultas dos fatos produto e ingresso criadas.

--- DW/CONSULTAS-FATO-INGRESSO.sql
+++ DW/CONSULTAS-FATO-INGRESSO.sql
@@ -1,71 +1,65 @@
 /* 1.Quantidade de ingressos vendidos por gênero de filme? */
 
-select sum(gr_filme), GENERO_FILME.gr_filme  as GENERO_FILME from dim_filme 
-group by gr_filme
-having sum(gr_filme) is not null
+select sum(GR_FILME) AS TOTAL, GENERO_FILME.gr_filme  as GENERO_FILME from DIM_FILME d
+INNER JOIN FATO_VENDA_INGRESSO v on (v.ID_FILME = d.ID_FILME)
+group by GR_FILME
+having sum(GR_FILME) is not null
 
-/* 2.Qual os dias, meses e anos que mais venderam ingressos? */
+/* 2.Quais os dias, meses e anos que mais venderam ingressos? */
 
-select sum(a.quantidade) as DIAS_COM_MAIS_VENDAS from fato_venda_ingresso a
+select sum(a.quantidade) as DIAS_COM_MAIS_VENDAS, B.DIA, B.MES, B.ANO from fato_venda_ingresso a
 inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-group by data
+group by DIA, MES, ANO
 having sum(a.quantidade) is not null
 
-select sum(a.quantidade) as DIAS_COM_MAIS_VENDAS from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-group by ano
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as DIAS_COM_MAIS_VENDAS from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-group by nome_mes
-having sum(a.quantidade) is not null
-
-/* 3.Quantidade de ingressos vendidos em determinados períodos por faixa etária? */
-
+/* 3.Qual o total de ingressos vendidos por filme e por período? */
+select i.id_fato_ingresso, f.nm_filme, sum(i.quantidade) as QUANTIDADE_TOTAL, t.DIA, t.mes, t.ano from fato_venda_ingresso i
+inner join dim_tempo t on (i.id_tempo = t.id_tempo)
+inner join dim_filme f on (t.id_filme = f.id_filme)
+group by DIA ,MES, ANO
+having sum(i.quantidade) is not null
 
 /* 4.Quantidade de ingressos vendidos por plataformas por período? */
 
-select sum(a.quantidade) as TOTAL_INGRESSOS_PRESENCIAL from fato_venda_ingresso a
+select sum(a.quantidade) as TOTAL_INGRESSOS_PRESENCIAL, B.DIA, B.MES ,B.ANO, C.TIPO_PLATAFORMA  from fato_venda_ingresso a
 inner join dim_tempo b on (a.id_tempo = b.id_tempo)
 inner join dim_plataforma c on (b.id_plataforma = c.id_plataforma)
-where b.ano = '2017' and c.tipo_plataforma = 'Presencial'
+GROUP BY DIA,MES,ANO, C.TIPO_PLATAFORMA
 having sum(a.quantidade) is not null
 
-select sum(a.quantidade) as TOTAL_INGRESSOS_PRESENCIAL from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-inner join dim_plataforma c on (b.id_plataforma = c.id_plataforma)
-where b.ano = '2018' and c.tipo_plataforma = 'Presencial'
-having sum(a.quantidade) is not null
+/* 6.Quais os filmes que mais venderam ingressos no ano? */
 
-select sum(a.quantidade) as TOTAL_INGRESSOS_ONLINE from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-inner join dim_plataforma c on (b.id_plataforma = c.id_plataforma)
-where b.ano = '2017' and c.tipo_plataforma = 'Online'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_INGRESSOS_ONLINE from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-inner join dim_plataforma c on (b.id_plataforma = c.id_plataforma)
-where b.ano = '2018' and c.tipo_plataforma = 'Online'
-having sum(a.quantidade) is not null
-
-/* 6.Quantidade de pessoas por sessões? */
-
-select sum(a.quantidade) as TOTAL_PESSOAS_SESSAO from fato_venda_ingresso a
-inner join dim_sessao b on (a.id_sessao = b.id_sessao)
-group by b.id_sessao
-having sum(a.quantidade) is not null
-
-/* 7.Quais os filmes que mais venderam ingressos no ano? */
-
-select top a.nm_filme as FILMES_MAIS_INGRESSOS from dim_filme a
+select top (5) a.nm_filme as FILMES from dim_filme a
 inner join fato_venda_ingresso b on (a.id_filme = b.id_filme)
 inner join dim_tempo c on (b.id_tempo = c.id_tempo)
 group by c.nm_filme
 
-/* 8.Qual o valor total de ingressos vendidos por dias, meses e anos? */
-/* 10.Qual o valor total de ingressos vendidos por plataformas em dias, meses e anos? */
-/* 11.Qual a quantidade de clientes por determinado bairro, cidade e estado? */
-/* 12.Quais os horários que mais venderam ingressos? */
-/* 13.Total de clientes por plano utilizado? */
+/* 7.Qual o valor total de ingressos vendidos por dias, meses e anos? */
+select sum(a.valor_pago) as VALOR_PAGO_TOTAL, t.DIA, t.MES, t.ANO from fato_venda_ingresso a
+inner join DIM_TEMPO as t on (t.ID_TEMPO = a.fato_venda_ingresso )
+group by DIA, MES, ANO
+having sum(a.valor_pago) is not null
+
+
+/* 9.Qual o valor total de ingressos vendidos por plataformas em dias, meses e anos? */
+select sum(a.valor_pago) as TOTAL_INGRESSOS_POR_PLATAFORMA, B.DIA, B.MES ,B.ANO, C.TIPO_PLATAFORMA  from fato_venda_ingresso a
+inner join dim_tempo b on (a.id_tempo = b.id_tempo)
+inner join dim_plataforma c on (b.id_plataforma = c.id_plataforma)
+GROUP BY DIA,MES,ANO, C.TIPO_PLATAFORMA
+having sum(a.quantidade) is not null
+
+/* 10.Qual a quantidade de clientes por determinado bairro, cidade e estado? */
+SELECT C.ID_CLIENTE, E.BAIRRO, E.CIDADE, E.ESTADO FROM DIM_CLIENTE C
+INNER JOIN FATO_VENDA_INGRESSO I ON (I.ID_CLIENTE = C.ID_CLIENTE)
+INNER JOIN DIM_ENDERECO_CLIENTE E ON (I.ID_ENDERECO = E.ID_ENDERECO)
+GROUP BY BAIRRO, CIDADE, ESTADO
+/* 11.Qual o turno que mais vendeu ingressos? */
+SELECT TOP 1 (I.ID_FATO_INGRESSO), T.TIPO FROM FATO_VENDA_INGRESSO I
+INNER JOIN DIM_TURNO T ON (I.ID_FATO_INGRESSO = T.ID_TURNO)
+GROUP BY TIPO
+/* 12.Total de clientes por plano utilizado? */
+SELECT SUM(I.QUANTIDADE) AS TOTAL_CLIENTE, C.ID_CLIENTE, P.ID_PLANO, P.TIPO_PLANO FROM FATO_VENDA_INGRESSO I
+INNER JOIN DIM_CLIENTE C ON (I.ID_CLIENTE = C.ID_CLIENTE)
+INNER JOIN DIM_PLANO P ON (I.ID_PLANO = P.ID_PLANO)
+GROUP BY TIPO_PLANO
+HAVING SUM(I.QUANTIDADE) IS NOT NULL
--- DW/CONSULTAS-FATO-PRODUTO.sql
+++ DW/CONSULTAS-FATO-PRODUTO.sql
@@ -7,205 +7,25 @@ select * from dim_plataforma
 select * from dim_produto
 select * from dim_pagamento
 select * from dim_turno
+select * from DIM_FILME
 
 select * from tb_plataforma
 
 /* 5.Qual o total de produtos vendidos em determinados períodos? */
 
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
+select sum(a.quantidade) AS TOTAL, b.mes, b.ano, c.NM_PRODUTO as NOME_DO_PRODUTO from fato_venda_produto a
 inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where data >= '20000101' and data <= '20091231'
+inner join dim_produto c on (a.id_produto = c.id_produto)
+--ORDER BY MES, ANO, NM_PRODUTO
+GROUP BY MES, ANO, NM_PRODUTO
 having sum(a.quantidade) is not null
 
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
+/* 8.Qual o valor total de produtos do snack bar vendidos por dias, meses e ano? */
+
+select P.TIPO_PLATAFORMA, B.ANO, B.DIA, B.MES, Sum(a.valor_pago) as VALOR_PAGO_TOTAL from fato_venda_produto a
 inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where data >= '20100101' and data <= '20181231'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Janeiro' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Fevereiro' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Março' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Abril' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Maio' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Junho' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Julho' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Agosto' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Setembro' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Outubro' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Novembro' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where nome_mes = 'Dezembro' and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where semestre = 1 and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where semestre = 2 and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where trimestre = 1 and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where trimestre = 2 and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where trimestre = 3 and ano <= '2018'
-having sum(a.quantidade) is not null
-
-select sum(a.quantidade) as TOTAL_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where trimestre = 4 and ano <= '2018'
-having sum(a.quantidade) is not null
-
-/* 9.Qual o valor total de produtos do snack bar vendidos por dias, meses e anos? */
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.ano = '2016'
+INNER JOIN DIM_PLATAFORMA P ON (P.ID_PLATAFORMA = A.ID_PLATAFORMA)
+WHERE TIPO_PLATAFORMA = 'PRESENCIAL'
+GROUP BY ANO, DIA, MES, TIPO_PLATAFORMA
 having sum(a.valor_pago) is not null
 
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.ano = '2017'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.ano = '2019'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.dia_semana = 'Sábado' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.dia_semana = 'Domingo' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Janeiro' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Fevereiro' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Março' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Abril' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Maio' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Junho' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Julho' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Agosto' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Setembro' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Outubro' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Novembro' and b.ano = '2018'
-having sum(a.valor_pago) is not null
-
-select sum(a.valor_pago) as VALOR_PAGO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-where b.nome_mes = 'Dezembro' and b.ano = '2018'
-having sum(a.valor_pago) is not null
