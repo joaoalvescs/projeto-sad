f1d7e5a041efa4d62516c67404e7f7d2a8e1254b
Clovijan <clovijan@gmail.com>
2019-08-18 21:16:52 -0300

    CRIAÇÃO CARGAS TABELAS AUX INGRESSO CRIAÇÃO DAS TABELAS DE VIOLAÇÃO INGRESSO

--- DW/PROCEDIMENTOS_CARGA_TB_AUXILIARES_INGRESSO.sql
+++ DW/PROCEDIMENTOS_CARGA_TB_AUXILIARES_INGRESSO.sql
@@ -4,12 +4,12 @@ CREATE PROCEDURE SP_OLTP_FATO_VENDA_INGRESSO(@data DATETIME)
 AS
 BEGIN
 	DECLARE @cod_compra INT, @id_ingresso INT, @id_endereco INT, @dt_venda DATE, @hora_venda TIME, @tipo_pag VARCHAR(25),
-			@id_cliente INT, @id_cinema INT, @id_plataforma INT, @id_filme INT, @id_sessao INT, @tipo_plano VARCHAR(45), @valor_compra NUMERIC(10,2)
+			@id_cliente INT, @id_cinema INT, @id_plataforma INT, @id_filme INT, @id_sessao INT, @id_plano INT, @valor_compra NUMERIC(10,2)
 
 	DELETE FROM TB_AUX_FATO_VENDA_INGRESSO WHERE DATA_CARGA = @data
 	
 	DECLARE C_FATO_VENDA CURSOR FOR SELECT COM.CODIGO_COMPRA, COM.DATA_COMPRA, COM.HORA_COMPRA, COM.VALOR_COMPRA, COM.ID_CINEMA, C.ID_CLIENTE,
-										   COM.ID_PLATAFORMA,E.ID_ENDERECO, CI.ID_INGRESSO, I.ID_FILME, I.ID_SESSAO, I.PLANO_USADO FROM TB_COMPRA AS COM
+										   COM.ID_PLATAFORMA,E.ID_ENDERECO, CI.ID_INGRESSO, I.ID_FILME, I.ID_SESSAO, I.ID_PLANO FROM TB_COMPRA AS COM
 										   INNER JOIN TB_CLIENTE AS C ON(COM.ID_CLIENTE = C.ID_CLIENTE) 
 										   INNER JOIN TB_ENDERECO AS E ON(C.ID_ENDERECO = E.ID_ENDERECO)
 										   INNER JOIN TB_COMPRA_INGRESSO AS CI ON(CI.ID_COMPRA = COM.ID_COMPRA)
@@ -17,16 +17,16 @@ BEGIN
 	OPEN C_FATO_VENDA
 
 	FETCH C_FATO_VENDA INTO @cod_compra, @dt_venda, @hora_venda, @valor_compra,@id_cinema, @id_cliente, @id_plataforma,
-							@id_endereco, @id_ingresso, @id_filme, @id_sessao, @
+							@id_endereco, @id_ingresso, @id_filme, @id_sessao, @id_plano
 
 	WHILE(@@FETCH_STATUS = 0)
 	BEGIN 
 		INSERT INTO TB_AUX_FATO_VENDA_INGRESSO
-		VALUES(@data, @cod_compra, @dt_venda, @hora_venda, @id_endereco, @id_produto, @cod_compra, @id_cliente, @id_cinema, @id_plataforma,
-			   @valor_compra, 1)
+		VALUES(@data, @cod_compra, @dt_venda, @hora_venda, @id_ingresso, @id_endereco, @cod_compra, @id_cliente, @id_cinema, @id_plataforma,
+			   @id_filme,@id_sessao, @id_plano, @valor_compra, 1)
 
 		FETCH C_FATO_VENDA INTO @cod_compra, @dt_venda, @hora_venda, @valor_compra,@id_cinema, @id_cliente, @id_plataforma,
-							@id_endereco, @id_produto
+							@id_endereco, @id_ingresso, @id_filme, @id_sessao, @id_plano
 	END
 	CLOSE C_FATO_VENDA
 	DEALLOCATE C_FATO_VENDA
@@ -52,21 +52,10 @@ BEGIN
 END
 
 -----------------------------CARGA PARA A TB_AUX_PLANO_USADO-------------------------------------------
-CREATE PROCEDURE SP_OLTP_TB_AUX_PLANO_USADO(@data DATETIME)
+CREATE PROCEDURE SP_OLTP_TB_AUX_PLANO (@data DATETIME)
 AS
 BEGIN
-	DECLARE @cod_ingresso INT, @plano VARCHAR(45)
-
-	DECLARE C_PLANO CURSOR FOR SELECT ID_INGRESSO, PLANO_USADO FROM TB_INGRESSO
-	OPEN C_PLANO 
-
-	FETCH C_PLANO INTO @cod_ingresso, @plano
-	WHILE(@@FETCH_STATUS = 0)
-		BEGIN
-			INSERT INTO TB_AUX_PLANO_USADO
-			VALUES(@data, @cod_ingresso, @plano)
-			FETCH C_PLANO INTO @cod_ingresso, @plano
-		END 
-	CLOSE C_PLANO
-	DEALLOCATE C_PLANO
+	DELETE FROM TB_AUX_PLANO WHERE DATA_CARGA = @data
+	INSERT INTO TB_AUX_PLANO
+	SELECT @data, ID_PLANO, TIPO_PLANO FROM TB_PLANO
 END
