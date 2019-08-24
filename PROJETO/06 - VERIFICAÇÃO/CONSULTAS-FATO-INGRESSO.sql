9c61f571344b84bf36285f6bd412ecadceca9a4d
Clovijan <clovijan@gmail.com>
2019-08-23 21:07:00 -0300

    CONSULTAS DOS AGREGADOS ADICIONADOS

--- PROJETO/06 - VERIFICAÇÃO/CONSULTAS-FATO-INGRESSO.sql
+++ PROJETO/06 - VERIFICAÇÃO/CONSULTAS-FATO-INGRESSO.sql
@@ -1,69 +1,86 @@
-/* Igor e João */
-USE CINEMA
-/* 1.Quantidade de ingressos vendidos por gênero de filme? */ -- OK
 
-select d.GR_FILME  as GENERO_FILME, sum(d.ID_FILME) AS TOTAL_INGRESSOS from DIM_FILME d
-INNER JOIN FATO_VENDA_INGRESSO v on (v.ID_FILME = d.ID_FILME)
-group by GR_FILME
-having sum(d.ID_FILME) is not null
+USE CINEMA
+GO
+
+/* 1.Quantidade de ingressos vendidos por gênero de filme? */ -- OK
+SELECT D.GR_FILME  AS 'GENERO FILME', SUM(d.ID_FILME) AS 'TOTAL DE INGRESSOS' 
+FROM DIM_FILME D
+INNER JOIN FATO_VENDA_INGRESSO V ON (V.ID_FILME = D.ID_FILME)
+GROUP BY GR_FILME
+HAVING SUM(D.ID_FILME) IS NOT NULL ORDER BY 'TOTAL DE INGRESSOS'
+
+
+
 
 /* 2.Quais os dias, meses e anos que mais venderam ingressos? */ -- OK
+SELECT SUM(A.QUANTIDADE) AS 'QUANTIDADE DE IGRESSOS', B.DIA_SEMANA AS DIA, B.NOME_MES AS MES, B.ANO 
+FROM FATO_VENDA_INGRESSO A
+INNER JOIN DIM_TEMPO B ON (A.ID_TEMPO = B.ID_TEMPO)
+GROUP BY DIA_SEMANA, NOME_MES, ANO
+HAVING SUM(A.QUANTIDADE) IS NOT NULL
 
-select sum(a.quantidade) as DIAS_COM_MAIS_VENDAS, B.DIA, B.MES, B.ANO from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-group by DIA, MES, ANO
-having sum(a.quantidade) is not null
 
-/* 3.Qual o total de ingressos vendidos por filme e por período? */ -- OK
-select f.NM_FILME,   t.DIA, t.MES, t.ANO, sum (i.QUANTIDADE) as QUANTIDADE_TOTAL from fato_venda_ingresso i
-inner join dim_tempo t on (i.id_tempo = t.id_tempo)
-inner join dim_filme f on (i.ID_FILME = f.id_filme)
-group by F.NM_FILME, DIA ,MES, ANO
-having sum(i.QUANTIDADE) is not null
+
+
+/* 3.Qual o total de ingressos vendidos por filme e por período? */ 
+SELECT F.NM_FILME AS FILME, T.DIA_SEMANA AS DIA, T.NOME_MES AS MES, T.ANO, SUM(I.QUANTIDADE) AS 'TOTAL DE INGRESSOS VENDIDOS' 
+FROM FATO_VENDA_INGRESSO I
+INNER JOIN DIM_TEMPO T ON (I.ID_TEMPO = T.ID_TEMPO)
+INNER JOIN DIM_FILME f on (I.ID_FILME = F.ID_FILME)
+GROUP BY F.NM_FILME, DIA_SEMANA , NOME_MES, ANO
+HAVING SUM(I.QUANTIDADE) IS NOT NULL
+
 
 /* 4.Quantidade de ingressos vendidos por plataformas por período? */ -- OK
-
-select sum(a.quantidade) as TOTAL_INGRESSOS_PRESENCIAL, B.DIA, B.MES ,B.ANO, C.TIPO_PLATAFORMA  from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-inner join dim_plataforma c on (a.id_plataforma = c.id_plataforma)
-GROUP BY DIA,MES,ANO, c.TIPO_PLATAFORMA
-having sum(a.quantidade) is not null
-
-/* 6.Quais os filmes que mais venderam ingressos no ano? */ --OK
-
-select top (3) a.NM_FILME as FILMES, b.quantidade as QUANTIDADE_INGRESSOS_VENDIDOS, p.TIPO_PLATAFORMA  from dim_filme a
-inner join fato_venda_ingresso b on (a.id_filme = b.ID_FILME)
-inner join dim_tempo c on (b.ID_TEMPO = c.ID_TEMPO)
-inner join DIM_PLATAFORMA p on (p.ID_PLATAFORMA = b.ID_PLATAFORMA)
-group by a.nm_filme, p.TIPO_PLATAFORMA, b.quantidade
-
-/* 7.Qual o valor total de ingressos vendidos por dias, meses e anos? */ -- OK
-select sum(a.valor_pago) as VALOR_PAGO_TOTAL, t.DIA, t.MES, t.ANO from fato_venda_ingresso a
-inner join DIM_TEMPO as t on (t.ID_TEMPO = a.ID_TEMPO )
-group by DIA, MES, ANO
-having sum(a.valor_pago) is not null
+SELECT SUM(A.QUANTIDADE) AS 'TOTAL INGRESSOS PRESENCIAL', B.DIA_SEMANA AS DIA, B.NOME_MES AS MES,
+B.ANO, C.TIPO_PLATAFORMA AS 'TIPO PLATAFORMA'  FROM FATO_VENDA_INGRESSO A
+INNER JOIN DIM_TEMPO B ON (A.ID_TEMPO = B.ID_TEMPO)
+INNER JOIN DIM_PLATAFORMA C ON (A.ID_PLATAFORMA = C.ID_PLATAFORMA)
+GROUP BY DIA_SEMANA,NOME_MES,ANO, C.TIPO_PLATAFORMA
+HAVING SUM(A.QUANTIDADE) IS NOT NULL
 
 
-/* 9.Qual o valor total de ingressos vendidos por plataformas em dias, meses e anos? */ -- OK
-select sum(a.valor_pago) as TOTAL_INGRESSOS_POR_PLATAFORMA, B.DIA, B.MES ,B.ANO, C.TIPO_PLATAFORMA  from fato_venda_ingresso a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-inner join dim_plataforma c on (a.id_plataforma = c.id_plataforma)
-GROUP BY DIA,MES,ANO, C.TIPO_PLATAFORMA
-having sum(a.quantidade) is not null
 
-/* 10.Qual a quantidade de clientes por determinado bairro, cidade e estado? */ -- OK
+/* 5.Quais os filmes que mais venderam ingressos no ano? */ --OK
+SELECT TOP(3) A.NM_FILME AS FILMES, B.QUANTIDADE AS 'QUANTIDADE INGRESSOS VENDIDOS', P.TIPO_PLATAFORMA AS PLATAFORMA  
+FROM DIM_FILME A
+INNER JOIN FATO_VENDA_INGRESSO B ON (A.ID_FILME = B.ID_FILME)
+INNER JOIN DIM_TEMPO C ON (B.ID_TEMPO = C.ID_TEMPO)
+INNER JOIN DIM_PLATAFORMA P ON (P.ID_PLATAFORMA = B.ID_PLATAFORMA)
+GROUP BY A.NM_FILME, P.TIPO_PLATAFORMA, B.QUANTIDADE
+
+
+
+/* 6.Qual o valor total de ingressos vendidos por dias, meses e anos? */ -- OK
+SELECT SUM(A.VALOR_PAGO) AS VALOR_PAGO_TOTAL, T.DIA, T.MES, T.ANO FROM FATO_VENDA_INGRESSO A
+INNER JOIN DIM_TEMPO AS T ON (T.ID_TEMPO = A.ID_TEMPO )
+GROUP BY DIA, MES, ANO
+HAVING SUM(A.VALOR_PAGO) IS NOT NULL
+
+
+/* 7.Qual o valor total de ingressos vendidos por plataformas em dias, meses e anos? */ -- OK
+
+SELECT SUM(A.VALOR_PAGO) AS 'VALOR DO INGRESSOS',
+B.DIA_SEMANA AS DIA, B.NOME_MES AS MES ,B.ANO, C.TIPO_PLATAFORMA AS PLATAFORMA 
+FROM FATO_VENDA_INGRESSO A
+INNER JOIN DIM_TEMPO B ON (A.ID_TEMPO = B.ID_TEMPO)
+INNER JOIN DIM_PLATAFORMA C ON (A.ID_PLATAFORMA = C.ID_PLATAFORMA)
+GROUP BY DIA_SEMANA, NOME_MES,ANO, C.TIPO_PLATAFORMA
+HAVING SUM(A.QUANTIDADE) IS NOT NULL
+
+/* 8.Qual a quantidade de clientes por determinado bairro, cidade e estado? */ -- OK
 SELECT  E.BAIRRO, E.CIDADE, E.ESTADO,SUM(C.ID_CLIENTE) AS Quantidade FROM DIM_CLIENTE C
 INNER JOIN FATO_VENDA_INGRESSO I ON (I.ID_CLIENTE = C.ID_CLIENTE)
 INNER JOIN DIM_ENDERECO_CLIENTE E ON (I.ID_ENDERECO = E.ID_ENDERECO)
 GROUP BY BAIRRO, CIDADE, ESTADO
 HAVING SUM(C.ID_CLIENTE) IS NOT NULL
 
-/* 11.Qual o turno que mais vendeu ingressos? */ -- OK
+/* 9.Qual o turno que mais vendeu ingressos? */ -- OK
 SELECT TOP 1 (T.TIPO), I.QUANTIDADE FROM FATO_VENDA_INGRESSO I
 INNER JOIN DIM_TURNO T ON (I.ID_FATO_INGRESSO = T.ID_TURNO)
 GROUP BY TIPO, I.QUANTIDADE
 
-/* 12.Total de clientes por plano de desconto utilizado? */ -- OK
+/* 10.Total de clientes por plano de desconto utilizado? */ -- OK
 SELECT P.TIPO_PLANO, SUM(C.ID_CLIENTE) AS TOTAL_CLIENTE FROM FATO_VENDA_INGRESSO I
 INNER JOIN DIM_CLIENTE C ON (I.ID_CLIENTE = C.ID_CLIENTE)
 INNER JOIN DIM_PLANO P ON (I.ID_PLANO = P.ID_PLANO)
--- PROJETO/06 - VERIFICAÇÃO/CONSULTAS-FATO-PRODUTO.sql
+++ PROJETO/06 - VERIFICAÇÃO/CONSULTAS-FATO-PRODUTO.sql
@@ -15,19 +15,18 @@ select * from tb_plataforma
 
 /* 5.Qual o total de produtos vendidos em determinados períodos? */
 
-select sum(a.quantidade) AS TOTAL, b.mes, b.ano, c.NM_PRODUTO as NOME_DO_PRODUTO from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
-inner join dim_produto c on (a.id_produto = c.id_produto)
---ORDER BY MES, ANO, NM_PRODUTO
+SELECT SUM(A.QUANTIDADE) AS TOTAL, B.MES, B.ANO, C.NM_PRODUTO AS NOME_DO_PRODUTO FROM FATO_VENDA_PRODUTO A
+INNER JOIN DIM_TEMPO B ON (A.ID_TEMPO = B.ID_TEMPO)
+INNER JOIN DIM_PRODUTO C ON (A.ID_PRODUTO = C.ID_PRODUTO)
 GROUP BY MES, ANO, NM_PRODUTO
-having sum(a.quantidade) is not null
+HAVING SUM(A.QUANTIDADE) IS NOT NULL
 
 /* 8.Qual o valor total de produtos do snack bar vendidos por dias, meses e ano? */
 
-select P.TIPO_PLATAFORMA, B.ANO, B.DIA, B.MES, Sum(a.valor_pago) as VALOR_PAGO_TOTAL from fato_venda_produto a
-inner join dim_tempo b on (a.id_tempo = b.id_tempo)
+SELECT P.TIPO_PLATAFORMA, B.ANO, B.DIA, B.MES, SUM(A.VALOR_PAGO) AS VALOR_PAGO_TOTAL FROM FATO_VENDA_PRODUTO A
+INNER JOIN DIM_TEMPO B ON(A.ID_TEMPO = B.ID_TEMPO)
 INNER JOIN DIM_PLATAFORMA P ON (P.ID_PLATAFORMA = A.ID_PLATAFORMA)
 WHERE TIPO_PLATAFORMA = 'PRESENCIAL'
 GROUP BY ANO, DIA, MES, TIPO_PLATAFORMA
-having sum(a.valor_pago) is not null
+HAVING SUM(A.VALOR_PAGO) IS NOT NULL
 
